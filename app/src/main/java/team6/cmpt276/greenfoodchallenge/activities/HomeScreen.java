dbe58ab9bb7472d9987fddd48c1b584b7c7dd567
Jan Ycasas <ycasas.jan@gmail.com>
2018-10-22 17:01:03 -0700

    revert back

--- app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/HomeScreen.java
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/HomeScreen.java
@@ -3,21 +3,6 @@ package team6.cmpt276.greenfoodchallenge.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import team6.cmpt276.greenfoodchallenge.R;
 
 public class HomeScreen extends AppCompatActivity {
@@ -26,25 +11,5 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-        PieChart chart = (PieChart) findViewById(R.id.chart);
-
-        List<PieEntry> entries = new ArrayList<PieEntry>();
-        entries.add(new PieEntry(14, "Fish"));
-        entries.add(new PieEntry(100, "Chicken"));
-        entries.add(new PieEntry(100, "Beef"));
-
-        PieDataSet dataSet = new PieDataSet(entries, "");
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        dataSet.setValueFormatter(new PercentFormatter());
-
-        chart.getLegend().setEnabled(false);
-        chart.getDescription().setEnabled(false);
-
-        PieData pieData = new PieData(dataSet);
-        pieData.setValueTextSize(10f);
-        chart.animateY(1500);
-        chart.setData(pieData);
-        chart.invalidate();
     }
 }
