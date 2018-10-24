5c3dc72daff3dea5dbd17c3db8ad764fc3da6038
hnn1 <hong_nhung_nguyen@sfu.ca>
2018-10-24 11:18:04 -0700

    Dynamically change Option List in Meat Eater Activities

--- app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/MeatEater.java
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/MeatEater.java
@@ -4,18 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import team6.cmpt276.greenfoodchallenge.R;
+import team6.cmpt276.greenfoodchallenge.classes.PlanPicker;
 
 public class MeatEater extends AppCompatActivity {
+
+    PlanPicker planPicker = new PlanPicker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meat_eater);
 
+        setupDisplay();
 
+        //Set up onClickListener for the option 1
         LinearLayout option1 = (LinearLayout)findViewById(R.id.option1);
 
         option1.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +38,28 @@ public class MeatEater extends AppCompatActivity {
         LinearLayout option2 = (LinearLayout) findViewById(R.id.option2);
 
         LinearLayout option3 = (LinearLayout)findViewById(R.id.option3);
+    }
+
+    // Set up the Option List according to user info
+    private void setupDisplay() {
+        ArrayList<Integer> displayOptionList = planPicker.getResource();
+
+        ImageView image1 = (ImageView) findViewById(R.id.image1);
+        image1.setImageResource(displayOptionList.get(0));
+
+        TextView text1 = (TextView) findViewById(R.id.text1);
+        text1.setText(displayOptionList.get(1));
+
+        ImageView image2 = (ImageView) findViewById(R.id.image2);
+        image2.setImageResource(displayOptionList.get(2));
+
+        TextView text2 = (TextView) findViewById(R.id.text2);
+        text2.setText(displayOptionList.get(3));
+
+        ImageView image3 = (ImageView) findViewById(R.id.image3);
+        image3.setImageResource(displayOptionList.get(4));
+
+        TextView text3 = (TextView) findViewById(R.id.text3);
+        text3.setText(displayOptionList.get(5));
     }
 }
---
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/classes/PlanPicker.java
@@ -0,0 +1,57 @@
+package team6.cmpt276.greenfoodchallenge.classes;
+
+import java.util.ArrayList;
+
+import team6.cmpt276.greenfoodchallenge.R;
+
+public class PlanPicker {
+    public PlanPicker(){};
+
+    public ArrayList<Integer> getResource() {
+        ArrayList<Integer> displayOptionList = new ArrayList<Integer>();
+        //Adding option 1
+        addResource(displayOptionList,"bean");
+        addResource(displayOptionList,"chicken");
+        addResource(displayOptionList,"pork");
+        return displayOptionList;
+    }
+
+    public void plantBased() {
+
+    }
+
+    public void meatEater() {
+
+    }
+
+    public void lowMeat() {
+
+    }
+
+    private void addResource (ArrayList<Integer> displayOptionList, String protein){
+        if (protein == "chicken"){
+            displayOptionList.add(R.drawable.chicken);
+            displayOptionList.add(R.string.chicken);
+        }
+        else if (protein == "pork") {
+            displayOptionList.add(R.drawable.pork);
+            displayOptionList.add(R.string.pork);
+        }
+        else if (protein == "fish") {
+            displayOptionList.add(R.drawable.fish);
+            displayOptionList.add(R.string.fish);
+        }
+        else if (protein == "egg") {
+            displayOptionList.add(R.drawable.egg);
+            displayOptionList.add(R.string.egg);
+        }
+        else {
+            displayOptionList.add(R.drawable.bean);
+            displayOptionList.add(R.string.bean);
+        }
+
+    }
+
+
+
+}
