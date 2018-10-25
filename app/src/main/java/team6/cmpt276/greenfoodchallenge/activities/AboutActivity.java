d41832f501934128e323e06e61de1925c2278cd6
hnn1 <hong_nhung_nguyen@sfu.ca>
2018-10-24 22:04:08 -0700

    Transfering AboutActivity and PopupActivity from mybranch

---
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/AboutActivity.java
@@ -0,0 +1,33 @@
+package team6.cmpt276.greenfoodchallenge.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import team6.cmpt276.greenfoodchallenge.R;
+
+public class AboutActivity extends AppCompatActivity {
+    Button learnMore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("About");
+
+        learnMore = findViewById(R.id.learn_more);
+        learnMore.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutActivity.this, PopupActivity.class));
+            }
+        });
+    }
+}
