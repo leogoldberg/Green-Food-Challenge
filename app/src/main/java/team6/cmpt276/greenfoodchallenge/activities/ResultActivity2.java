a9e14364bcf077de9bf43c9b54ddd8885caa1914
Jan Ycasas <ycasas.jan@gmail.com>
2018-10-25 10:47:16 -0700

    fix layout

--- app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/ResultActivity2.java
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/ResultActivity2.java
@@ -3,6 +3,7 @@ package team6.cmpt276.greenfoodchallenge.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,6 +30,11 @@ public class ResultActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result2);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Your Suggested Diet");
 
         Intent intent = getIntent();
 
