cfdc7723f7042c2b675fbec7d66d7ade3cc826c1
Jan Ycasas <ycasas.jan@gmail.com>
2018-10-25 00:07:47 -0700

    finished the first results activity

--- app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/ResultActivity.java
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/ResultActivity.java
@@ -8,15 +8,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import team6.cmpt276.greenfoodchallenge.R;
 import team6.cmpt276.greenfoodchallenge.classes.UserData;
 
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -25,31 +22,11 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 
 public class ResultActivity extends AppCompatActivity {
-
-    private static final int PORK = 0;
-    private static final int BEEF = 1;
-    private static final int CHICKEN = 2;
-    private static final int FISH = 3;
-    private static final int EGGS = 4;
-    private static final int BEANS = 5;
-    private static final int VEGETABLES = 6;
-    private static final int NUM_OF_ITEMS = 7;
-
-    /*private ProteinSource[] proteinItems = {
-            new ProteinSource("Beef", 27),
-            new ProteinSource("Pork", 12.1),
-            new ProteinSource("Chicken", 6.9),
-            new ProteinSource("Fish", 6.1),
-            new ProteinSource("Eggs", 4.8),
-            new ProteinSource("Beans", 2),
-            new ProteinSource("Vegetables", 2)
-    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,85 +44,84 @@ public class ResultActivity extends AppCompatActivity {
             }
         });
 
+        // gets the intent variables
         Intent intent = getIntent();
         int protein_per_meal = intent.getIntExtra("protein_per_meal", -1);
         int total_amount_per_week = intent.getIntExtra("total_amount_per_week", -1);
-
         HashMap<String, Integer> times_per_week = (HashMap<String, Integer>)intent.getSerializableExtra("times_per_week");
 
-        PieChart chart = findViewById(R.id.chart);
+        // sets up the pie charts
+        PieChart chart = initializePieChart(R.id.chart);
 
         List<PieEntry> entries = addEntries(times_per_week, total_amount_per_week);
+        PieDataSet dataSet = setPieDataSet(entries);
 
-        PieDataSet dataSet = new PieDataSet(entries, "");
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        dataSet.setValueFormatter(new PercentFormatter());
+        PieData pieData = setPieData(dataSet);
 
-        chart.getLegend().setEnabled(false);
-        chart.getDescription().setEnabled(false);
-
-        PieData pieData = new PieData(dataSet);
-        pieData.setValueTextSize(10f);
-        chart.animateY(1500);
         chart.setData(pieData);
         chart.invalidate();
 
+        // set the frequency
         UserData userData = new UserData(protein_per_meal,0);
         userData = setProteinFrequency(userData, times_per_week);
 
+        // set total text
         double total = userData.getTotalco2();
-
-        Log.i("ResultsActivity", "protein_per_meal: " + protein_per_meal);
-
-        /*Consumption myConsumption = new Consumption();
-        for(int i = 0; i < NUM_OF_ITEMS; i++){
-            FoodData item = new FoodData(proteinItems[i], times_per_week[i], protein_per_meal);
-            myConsumption.addItem(item);
-        }
-        //example
-        double pork_co2e = myConsumption.getItemCO2e(PORK);
-        double totalco2e = myConsumption.getTotalCO2e();
-        Log.i("ResultsActivity", "pork_co2e: " + pork_co2e);
-        Log.i("ResultsActivity", "totalco2e: " + totalco2e);
-        */
+        TextView tv1 = findViewById(R.id.consumed_co2e);
+        tv1.setText(Double.toString(total) + " CO2e");
     }
 
-    public List<PieEntry> addEntries(HashMap<String, Integer> times_per_week,
+    private List<PieEntry> addEntries(HashMap<String, Integer> times_per_week,
                                      int total_amount_per_week) {
         List<PieEntry> entries = new ArrayList<>();
 
-        Iterator it = times_per_week.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-
-            String key = (String) pair.getKey();
-            int value = (int) pair.getValue();
+        for (Map.Entry<String, Integer> entry : times_per_week.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
             float percentage = getPercentage(value, total_amount_per_week);
 
             entries.add(new PieEntry(percentage, key));
-            it.remove();
         }
 
         return entries;
     }
 
-    public float getPercentage(int count, int total) {
+    private float getPercentage(int count, int total) {
         return ((float) count / total) * 100;
     }
 
-    public UserData setProteinFrequency(UserData userData, HashMap<String, Integer> times_per_week) {
-        Iterator it = times_per_week.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-
-            String key = (String) pair.getKey();
-            int value = (int) pair.getValue();
+    private UserData setProteinFrequency(UserData userData, HashMap<String, Integer> times_per_week) {
+        for (Map.Entry<String, Integer> entry : times_per_week.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
 
             userData.setFoodFrequency(key, value);
-
-            it.remove();
         }
 
         return userData;
+    }
+
+    private PieDataSet setPieDataSet(List<PieEntry> entries) {
+        PieDataSet dataSet = new PieDataSet(entries, "");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setValueFormatter(new PercentFormatter());
+
+        return dataSet;
+    }
+
+    private PieChart initializePieChart(int id) {
+        PieChart chart = findViewById(id);
+        chart.getLegend().setEnabled(false);
+        chart.getDescription().setEnabled(false);
+        chart.animateY(1500);
+
+        return chart;
+    }
+
+    private PieData setPieData(PieDataSet dataSet) {
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueTextSize(10f);
+
+        return pieData;
     }
 }
