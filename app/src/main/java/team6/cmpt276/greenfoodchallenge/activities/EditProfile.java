297b54326e9e32f598a8e38c6f3b76a71b7d3068
lgoldber <lgoldber@sfu.ca>
2018-11-07 19:49:27 -0800

    "setting up ui"

---
+++ app/src/main/java/team6/cmpt276/greenfoodchallenge/activities/EditProfile.java
@@ -0,0 +1,16 @@
+package team6.cmpt276.greenfoodchallenge.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team6.cmpt276.greenfoodchallenge.R;
+
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
